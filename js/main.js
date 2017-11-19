2eb4060c8a8e5f23a0c86a575393397ccd582c87
Philip Savarirayan <philip.sav@gmail.com>
2017-11-19 20:11:57 +0530

    5

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//header shrink
 $(function(){
  var shrinkHeader = 150;
   $(window).scroll(function() {
@@ -15,67 +16,67 @@ function getCurrentScroll() {
 });
 
 
-///scroll top
-// BY KAREN GRIGORYAN
-
-$(document).ready(function() {
-  /******************************
-      BOTTOM SCROLL TOP BUTTON
-   ******************************/
-
-  // declare variable
-  var scrollTop = $(".scrollTop");
-
-  $(window).scroll(function() {
-    // declare variable
-    var topPos = $(this).scrollTop();
-
-    // if user scrolls down - show scroll to top button
-    if (topPos > 100) {
-      $(scrollTop).css("opacity", "1");
-
-    } else {
-      $(scrollTop).css("opacity", "0");
-    }
-
-  }); // scroll END
-
-  //Click event to scroll to top
-  $(scrollTop).click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 800);
-    return false;
-
-  }); // click() scroll top EMD
-
-  
+//scrolltop
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
 });
 
 ///headroom
-	var MQL = 1170;
+var headroom = new Headroom(header, {
+  "offset": 205,
+  "tolerance": 5,
+  "classes": {
+    "initial": "animated",
+    "pinned": "slideDown",
+    "unpinned": "slideUp"
+  }
+});
+headroom.init();
 
-if($(window).width() > MQL) {
-		var headerHeight = $('.cd-header').height();
-		$(window).on('scroll',
-		{
-	        previousTop: 0
-	    }, 
-	    function () {
-		    var currentTop = $(window).scrollTop();
-		    //check if user is scrolling up
-		    if (currentTop < this.previousTop ) {
-		    	//if scrolling up...
-		    	if (currentTop > 0 && $('.cd-header').hasClass('is-fixed')) {
-		    		$('.cd-header').addClass('is-visible');
-		    	} else {
-		    		$('.cd-header').removeClass('is-visible is-fixed');
-		    	}
-		    } else {
-		    	//if scrolling down...
-		    	$('.cd-header').removeClass('is-visible');
-		    	if( currentTop > headerHeight && !$('.cd-header').hasClass('is-fixed')) $('.cd-header').addClass('is-fixed');
-		    }
-		    this.previousTop = currentTop;
-		});
-	}
+// to destroy
+headroom.destroy();
+//toggle
+$(function(){
+$('.toggleNav').on('click',function(event){
+    event.preventDefault();
+    $('nav ul').toggleClass('open');                
+    });
+});
+
+//scrollto
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900);
+	});
+});
+
+//padding
+$(function(){
+
+ var hh = $('.header').height(); 
+    
+    $('body').css('padding-top',hh);
+    
+});
